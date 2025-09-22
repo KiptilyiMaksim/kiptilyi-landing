46d26e2f3382bb0bd1fdd3a6bfa549166a67a52c
KiptilyiMaksim <maksimkiptily@gmail.com>
2025-09-22 17:53:41 +0300

    finished burger-menu

--- js/main.js
+++ js/main.js
@@ -6,5 +6,13 @@ burgerMenu.addEventListener('click', function () {
 	bodyFilter.classList.toggle('burger-body__open');
 	burgerMenu.classList.toggle('burger__open');
 	menuList.classList.toggle('burger-list__open');
-
+});
+document.addEventListener('click', function (removeList) {
+	if (bodyFilter.classList.contains('burger-body__open') &&
+		!removeList.target.closest('#toggleBurger') &&
+		!removeList.target.closest('#toggleBurgerList')) {
+		bodyFilter.classList.remove('burger-body__open');
+		burgerMenu.classList.remove('burger__open');
+		menuList.classList.remove('burger-list__open');
+	}
 });
