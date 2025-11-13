7cc2c6be9bf09baf70ce0384ef255427deae0ef8
KiptilyiMaksim <maksimkiptily@gmail.com>
2025-11-13 12:31:52 +0200

    bag fix on header menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 const burgerMenu = document.querySelector('#toggleBurger');
 const menuList = document.querySelector('#toggleBurgerList');
 const bodyFilter = document.querySelector('#toggleBurgerBody');
+const menuLinks = document.querySelectorAll('.header a');
 
 burgerMenu.addEventListener('click', function () {
 	bodyFilter.classList.toggle('burger-body__open');
 	burgerMenu.classList.toggle('burger__open');
 	menuList.classList.toggle('burger-list__open');
+});
+menuLinks.forEach(link => {
+	link.addEventListener('click', () => {
+		bodyFilter.classList.remove('burger-body__open');
+		burgerMenu.classList.remove('burger__open');
+		menuList.classList.remove('burger-list__open');
+	});
 });
 document.addEventListener('click', function (removeList) {
 	if (bodyFilter.classList.contains('burger-body__open') &&
